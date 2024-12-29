4d73577382695339f34e5afa6cde45d656c0c561
pulkit777exe <pulkitpareek88@gmail.com>
2024-12-29 23:19:25 +0530

    fixed production issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,16 @@
-import { useEffect } from "react";
-import {BrowserRouter,Routes,Route} from "react-router"
-import { Approach } from "../page/approach";
-import { Footer } from "../page/footer";
-import { Hero } from "../page/hero";
-import { Work } from "../page/work";
-import { useState } from "react";
-import { Loader } from "lucide-react";
+import { useEffect,useState } from "react";
+import {BrowserRouter,Routes,Route} from "react-router";
 import SignUp from "../page/signup";
+import { Home } from "../page/home";
 
 export default function App(){
-      const [ loading,setLoading ] = useState(true);
-      useEffect(()=>{
-        setTimeout(()=>{
-          setLoading(false)
-        },1000)
-      },[]);
-
-
-  // return (<div>
-  //   {loading ? (
-  //     <div className="flex items-center justify-center h-screen">
-  //       <Loader className="animate-spin w-12 h-12 text-yellow-500" />
-  //     </div>
-  //   ) : ( <>
-  //       <Hero/>
-  //       <Approach/>
-  //       <Work/>
-  //       <Footer/>
-  //     </>
-  //   )}
-  // </div>)
+      
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<>
-            <Hero/>
-            <Approach/>
-            <Work/>
-            <Footer/>
-          </>}/>
-          <Route path="user/signup" element={<SignUp/>}/>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/user/signup" element={<SignUp/>}/>
         </Routes>
       </BrowserRouter>
     </div>      
